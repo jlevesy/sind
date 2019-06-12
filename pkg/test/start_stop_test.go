8d2c34913a03398bedb7dc76aa2509f3a7e3eb75
Julien Levesy <jlevesy@gmail.com>
2019-06-12 21:14:24 +0200

    add an integration test for stop and start

---
+++ pkg/test/start_stop_test.go
@@ -0,0 +1,63 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	docker "github.com/docker/docker/client"
+	"github.com/jlevesy/sind/pkg/sind"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSindCanStopAndStartACluster(t *testing.T) {
+	ctx := context.Background()
+
+	hostClient, err := docker.NewClientWithOpts(docker.FromEnv, docker.WithVersion("1.39"))
+	require.NoError(t, err)
+
+	params := sind.ClusterConfiguration{
+		ClusterName: "test_create",
+		NetworkName: "test_create",
+
+		Managers: 3,
+		Workers:  4,
+	}
+	require.NoError(t, sind.CreateCluster(ctx, hostClient, params))
+
+	defer func() {
+		require.NoError(t, sind.DeleteCluster(ctx, hostClient, params.ClusterName))
+	}()
+
+	require.NoError(t, sind.StopCluster(ctx, hostClient, params.ClusterName))
+
+	clusterInfos, err := sind.InspectCluster(ctx, hostClient, params.ClusterName)
+	require.NoError(t, err)
+
+	for _, node := range clusterInfos.Nodes {
+		assert.Equal(t, "exited", node.State)
+	}
+
+	require.NoError(t, sind.StartCluster(ctx, hostClient, params.ClusterName))
+
+	clusterInfos, err = sind.InspectCluster(ctx, hostClient, params.ClusterName)
+	require.NoError(t, err)
+
+	for _, node := range clusterInfos.Nodes {
+		assert.Equal(t, "running", node.State)
+	}
+
+	swarmHost, err := sind.ClusterHost(ctx, hostClient, params.ClusterName)
+	require.NoError(t, err)
+
+	swarmClient, err := docker.NewClientWithOpts(docker.WithHost(swarmHost), docker.WithVersion("1.39"))
+	require.NoError(t, err)
+
+	info, err := swarmClient.Info(ctx)
+	require.NoError(t, err)
+
+	require.True(t, info.Swarm.ControlAvailable)
+
+	assert.EqualValues(t, params.Managers, info.Swarm.Managers)
+	assert.EqualValues(t, params.Workers, info.Swarm.Nodes-info.Swarm.Managers)
+}
