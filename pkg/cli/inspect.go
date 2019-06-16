4d5513ebb2f83736ae3b2879839bf6f8ffafecb1
Julien Levesy <jlevesy@gmail.com>
2019-06-16 11:55:29 +0200

    add sind inspect command

---
+++ pkg/cli/inspect.go
@@ -0,0 +1,53 @@
+package cli
+
+import (
+	"context"
+	"os"
+	"syscall"
+
+	docker "github.com/docker/docker/client"
+	"github.com/jlevesy/sind/pkg/cli/internal"
+	"github.com/jlevesy/sind/pkg/sind"
+	"github.com/spf13/cobra"
+	"github.com/ullaakut/disgo"
+)
+
+var (
+	inspectCmd = &cobra.Command{
+		Use:   "inspect",
+		Short: "Inspect a specific cluster.",
+		Run:   runInspect,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(inspectCmd)
+}
+
+func runInspect(cmd *cobra.Command, args []string) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	ctx, cancel = internal.WithSignal(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	disgo.StartStep("Connecting to the docker daemon")
+	client, err := docker.NewClientWithOpts(docker.FromEnv, docker.WithVersion("1.39"))
+	if err != nil {
+		fail(disgo.FailStepf("Unable to connect to the docker daemon: %v", err))
+	}
+
+	disgo.StartStepf("Checking if a cluster named %q already exists", clusterName)
+	clusterInfo, err := sind.InspectCluster(ctx, client, clusterName)
+	if err != nil {
+		fail(disgo.FailStepf("Unable to check if the cluster already exists: %v", err))
+	}
+
+	if clusterInfo == nil {
+		fail(disgo.FailStepf("Cluster %q does not exists", clusterName))
+	}
+
+	disgo.EndStep()
+
+	internal.RenderCluster(os.Stdout, *clusterInfo)
+}
